1d9a00014865b6bced87a83cac8706d0ca0d4917
Luka <luka.janketic@gmail.com>
2021-08-31 00:40:09 +0200

    commented out unfinished things

--- methods/noteMethods.js
+++ methods/noteMethods.js
@@ -10,31 +10,31 @@ module.exports.createNote = async (noteData) => {
         priority: noteData.priority,
         nature: noteData.nature,
         description: noteData.description
-    });
+    })
     try {
-        await newNote.save();
-        return 'NOTE POST OK';
+        await newNote.save()
+        return 'NOTE POST OK'
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 };
 
 module.exports.deleteNote = async (noteData) => {
     const myQuery = { user: noteData.user, title: noteData.title, date: noteData.date, description: noteData.description }
     try {
-        await Note.deleteOne(myQuery);
-        return 'DELETE OK';
+        await Note.deleteOne(myQuery)
+        return 'DELETE OK'
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 };
 
-module.exports.updateNote = async (noteData) => {
-    const myQuery = {};
-    const newValues = {};
+/* module.exports.updateNote = async (noteData) => {
+    const myQuery = {}
+    const newValues = {}
     try {
 
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
-}
+} */
